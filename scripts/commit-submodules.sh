48c85ea9fffdfcfedc4e0a519413d47b31bc9d5e
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-09-16 17:05:02 +0200

    Add commit-submodules.sh

---
+++ scripts/commit-submodules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Commits each submodule. stdout is disabled for ignoring errors,
+# keep that in mind.
+
+cd ..
+
+git add codewars-brainfuck
+git commit -m "Update brainfuck repo" 1>/dev/null
+git add codewars-c 1>/dev/null
+git commit -m "Update codewars-c repo" 1>/dev/null
+git add codewars-python 1>/dev/null
+git commit -m "Update codewars-python repo" 1>/dev/null
+git add codewars-rust 1>/dev/null
+git commit -m "Update codewars-rust repo" 1>/dev/null
+git add codewars-shell 1>/dev/null
+git commit -m "Update codewars-shell repo" 1>/dev/null
